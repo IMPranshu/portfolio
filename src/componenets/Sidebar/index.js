e221846a306eb5d1236ce6c2f2cbdbccda34dec3
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-29 21:51:27 +0530

    Personal Logo added

---
+++ src/componenets/Sidebar/index.js
@@ -0,0 +1,21 @@
+import './index.scss';
+
+import {Link} from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+
+const Sidebar = () => (
+
+  <div className='nav-bar'>
+    <Link className='logo' to='/'>
+      <img src={LogoS} alt="logo" />
+      <img src={LogoSubtitle} alt="pk-agrawal" />
+
+    
+    </Link>
+
+  </div>
+)
+
+export default Sidebar
